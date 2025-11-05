4075da29e4364781b49f140dc5548ff3b09b992d
SbGit05 <155940139+SbGit05@users.noreply.github.com>
2025-11-05 16:00:40 +0530

    Update 123B1F006_Assignment_3.c

--- 123B1F006_Assignment_3.c
+++ 123B1F006_Assignment_3.c
@@ -1,3 +1,23 @@
+// Scenario: Emergency Relief Supply Distribution 
+// A devastating flood has hit multiple villages in a remote area, and the government, along 
+// with NGOs, is organizing an emergency relief operation. A rescue team has a limited
+// capacity boat that can carry a maximum weight of W kilograms. The boat must transport 
+// critical supplies, including food, medicine, and drinking water, from a relief center to the 
+// affected villages. 
+// Each type of relief item has: 
+// ● A weight (wi) in kilograms. 
+// ● Utility value (vi) indicating its importance (e.g., medicine has higher value than food). 
+// ● Some items can be divided into smaller portions (e.g., food and water), while others must 
+// be taken as a whole (e.g., medical kits). 
+// As the logistics manager, you must: 
+// 1. Implement the Fractional Knapsack algorithm to maximize the total utility value of the 
+// supplies transported. 
+// 2. Prioritize high-value items while considering weight constraints. 
+// 3. Allow partial selection of divisible items (e.g., carrying a fraction of food packets). 
+// 4. Ensure that the boat carries the most critical supplies given its weight limit W. 
+
+//Shreedhar Bargaje- 123B1F006
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -80,4 +100,5 @@ int main() {
     printf("\nTotal Maximum Utility Achieved: %.2f\n", max_utility);
 
     return 0;
+
 }
